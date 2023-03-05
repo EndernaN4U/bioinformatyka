b5aad4e927db7fb35531991e4d1a976080c28599
AreczeQ192 <97028504+AreczeQ192@users.noreply.github.com>
2023-03-05 22:34:06 +0100

    Update Solver.tsx

--- src/components/Solver.tsx
+++ src/components/Solver.tsx
@@ -39,29 +39,82 @@ export default function Solver() {
 
   return (
     <div>
-        <label>Text</label>
-        <input type="radio" name="type" checked={(type==="text")} onClick={()=>setType("text")}/>
+        <main>
+            <div className="container">
+                <div className="about">
+                    <h2>Protein analysis</h2>
+                    <p>
+                        The app generates the structure<br />
+                        and data of a protein, where output<br />
+                        bases on the amino acid string.
+                    </p>
+                </div>
+                <div className="input-section">
+                    <div className='inputDiv'>
+                        <label>Text</label>
+                        <input type="radio" name="type" checked={(type==="text")} onClick={()=>setType("text")}/>
+                    </div>
+                   
+                    <div className='inputDiv'>
+                        <label>File</label>
+                        <input type="radio" name="type" checked={(type==="file")} onClick={()=>setType("file")}/>
+                    </div>
 
-        <label>File</label>
-        <input type="radio" name="type" checked={(type==="file")} onClick={()=>setType("file")}/>
+                    <div style={{display: (type==="file")? "block":"none"}} className='inputDiv'>
+                    <input ref={fileRef} type="file" accept='.txt' multiple={false}/>
+                    </div>
 
-        <textarea style={{display: (type==="text")? "block":"none"}} ref={textRef}></textarea>
-        <input style={{display: (type==="file")? "block":"none"}} ref={fileRef} type="file" accept='.txt' multiple={false}/>
-        <button onClick={changeData}>Calculate</button>
-         {
-            inf.data?
-            <div>
-                {
-                    inf.data.map((rna: any, ind: number)=>{
-                        const data = inf.getProperties(rna.sequence);
-                        return (
-                            <Props data={data} inf={inf} key={ind}/>
-                        ) ; 
-                    })
-                }
+                    <textarea 
+                        style={{display: (type==="text")? "block":"none"}} 
+                        ref={textRef} 
+                        placeholder="Enter the amino acid string" 
+                        id="here" 
+                        onInput={changeData}>
+                    </textarea>
+                    <button onClick={changeData}>Calculate</button>
+
+                </div>
             </div>
-            :<></>
-         }
+
+            <div className='proteinContainer'>
+            {
+                inf.data?
+                <div>
+                    {
+                        inf.data.map((rna: any, ind: number)=>{
+                            const data = inf.getProperties(rna.sequence);
+                            return (
+                                <Props data={data} inf={inf} key={ind}/>
+                            ) ; 
+                        })
+                    }
+                </div>
+                :<></>
+            }
+            </div>
+        </main>
+        
+
+        <footer>
+            <div className="footer-authors">
+            <figure>
+                <figcaption>Designed by:</figcaption>
+                <ul>
+                    <li>Arkadiusz Skupień</li>
+                    <li>Adrian Ziembla</li>
+                    <li>Karol Marek</li>
+                    <li>Maciej Matuszczyk</li>
+                    <li>Sebastian Rogóż</li>
+                </ul>
+            </figure>
+            </div>
+            <div className="footer-doc">
+                <b><a href="#">Download documentation</a></b>
+            </div> 
+            <div className="footer-copyright">
+                &copy; orłyZSŁ
+            </div>
+        </footer>
     </div>
   )
 }
